e24bdcf46d51aed2a71c151309e05a960dbf2186
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-09 18:07:28 +0500

    Working Hello World with TypeScript & Tailwind CSS

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+
+export default class DenoWebsiteApp extends App {
+    render() {
+        const { Component, pageProps } = this.props;
+        return (
+            <div className="h-screen">
+                <Head ><title/></Head>
+                <Component {...pageProps} />
+            </div>
+        );
+    }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+               <title>Genemator</title>
+            </Head>
+            <div>
+                Hello World
+            </div>
+        </>
+    )
+}
