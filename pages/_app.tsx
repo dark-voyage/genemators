07b127a0242a5a7f909600e0d0829a2aeee090ab
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-09 19:41:18 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,15 @@ import "../components/app.css";
 export default class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props;
+        const preview: string = `https://raw.githubusercontent.com/genemators/genemators/master/public/preview.png`
         return (
             <div className="h-screen">
                 <Head>
                     <meta property="og:title" content="Genemator's Portfolio Website!"/>
-                    <meta property="og:image" content=""/>
-                    <meta property="og:site_name" content="Genemator's"/>
+                    <meta property="og:image" content={preview}/>
+                    <meta property="og:site_name" content="Genemator's Portfolio"/>
                     <meta property="og:description" content="Website created by Genemator Sakhib to present him and his works"/>
-                    <title>|| Genemator's Portfolio</title>
+                    <title>Genemator's Portfolio</title>
                 </Head>
                 <Component {...pageProps} />
             </div>
