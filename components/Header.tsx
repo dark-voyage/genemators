b138a1705d414464dd3d4bfbd48b831991e04bdb
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-14 02:08:50 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,12 +10,32 @@ const Header = ({ subtitle }: { subtitle: string }) => {
 
     const path = useRouter().pathname;
 
-    const homeDetect = () => {
+    const homeDetectMenu = () => {
         if (path !== "/") {
             return(
                 <Link href="/">
                     <a className="text-base leading-6 font-medium text-white hover:text-gray-200 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150">
                         Home
+                    </a>
+                </Link>
+            )
+        }
+    }
+
+    const homeDetectResponsive = () => {
+        if (path !== "/") {
+            return(
+                <Link href="/">
+                    <a
+                        className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
+                        <svg className="flex-shrink-0 h-6 w-6" fill="none"
+                             viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+                        </svg>
+                        <div className="text-base leading-6 font-medium">
+                            Home
+                        </div>
                     </a>
                 </Link>
             )
@@ -48,7 +68,7 @@ const Header = ({ subtitle }: { subtitle: string }) => {
                     </div>
                     <nav className="hidden md:flex space-x-10">
 
-                        {homeDetect()}
+                        {homeDetectMenu()}
                         <Link href="/posts">
                             <a className="text-base leading-6 font-medium text-white hover:text-gray-200 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150">
                                 Posts
@@ -428,61 +448,36 @@ const Header = ({ subtitle }: { subtitle: string }) => {
                             </div>
                             <div>
                                 <nav className="grid row-gap-8">
-                                    <a href="#"
-                                       className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
-                                        <svg className="flex-shrink-0 h-6 w-6" fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
-                                        </svg>
-                                        <div className="text-base leading-6 font-medium">
-                                            Analytics
-                                        </div>
-                                    </a>
-                                    <a href="#"
-                                       className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
-                                        <svg className="flex-shrink-0 h-6 w-6" fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                  d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"/>
-                                        </svg>
-                                        <div className="text-base leading-6 font-medium">
-                                            Engagement
-                                        </div>
-                                    </a>
-                                    <a href="#"
-                                       className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
-                                        <svg className="flex-shrink-0 h-6 w-6" fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"/>
-                                        </svg>
-                                        <div className="text-base leading-6 font-medium">
-                                            Security
-                                        </div>
-                                    </a>
-                                    <a href="#"
-                                       className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
-                                        <svg className="flex-shrink-0 h-6 w-6" fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                  d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"/>
-                                        </svg>
-                                        <div className="text-base leading-6 font-medium">
-                                            Integrations
-                                        </div>
-                                    </a>
-                                    <a href="#"
-                                       className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
-                                        <svg className="flex-shrink-0 h-6 w-6" fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                  d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>
-                                        </svg>
-                                        <div className="text-base leading-6 font-medium">
-                                            Automations
-                                        </div>
-                                    </a>
+                                    {homeDetectResponsive()}
+                                    <Link href="/posts">
+                                        <a
+                                           className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
+                                            <svg className="flex-shrink-0 h-6 w-6" fill="none"
+                                                 viewBox="0 0 24 24" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                      d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                      d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/>
+                                            </svg>
+                                            <div className="text-base leading-6 font-medium">
+                                                Posts
+                                            </div>
+                                        </a>
+                                    </Link>
+                                    <Link href="/about">
+                                        <a
+                                           className="-m-3 p-3 flex items-center space-x-3 rounded-md text-white hover:bg-white hover:text-black transition ease-in-out duration-150">
+                                            <svg className="flex-shrink-0 h-6 w-6" fill="none"
+                                                 viewBox="0 0 24 24" stroke="currentColor">
+                                                <circle strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" cx="12" cy="12" r="10"/>
+                                                <line strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" y1="8" x2="12" y2="12"/>
+                                                <line strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" y1="16" x2="12.01" y2="16"/>
+                                            </svg>
+                                            <div className="text-base leading-6 font-medium">
+                                                About
+                                            </div>
+                                        </a>
+                                    </Link>
                                 </nav>
                             </div>
                         </div>
