f13eb7baf8ac46a61e7efde3ee013ee83e4ceec5
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-17 11:02:45 +0500

    preact coverage

--- next.config.js
+++ next.config.js
@@ -1,9 +1,41 @@
-const withPWA = require('next-pwa')
+const withPrefresh = require("@prefresh/next");
 
-module.exports = withPWA({
-    pwa: {
-        register: true,
-        sw: 'service-worker.js',
-        //...
-    }
-})
+module.exports = withPrefresh({
+    experimental: {
+        modern: true,
+        polyfillsOptimization: true
+    },
+    webpack(config, { dev, isServer }) {
+        const splitChunks = config.optimization && config.optimization.splitChunks;
+        if (splitChunks) {
+            const cacheGroups = splitChunks.cacheGroups;
+            const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+            if (cacheGroups.framework) {
+                cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+                    test: preactModules,
+                });
+                cacheGroups.commons.name = "framework";
+            } else {
+                cacheGroups.preact = {
+                    name: "commons",
+                    chunks: "all",
+                    test: preactModules,
+                };
+            }
+        }
+
+        // inject Preact DevTools
+        if (dev && !isServer) {
+            const entry = config.entry;
+            config.entry = () =>
+                entry().then((entries) => {
+                    entries["main.js"] = ["preact/debug"].concat(
+                        entries["main.js"] || []
+                    );
+                    return entries;
+                });
+        }
+
+        return config;
+    },
+});
