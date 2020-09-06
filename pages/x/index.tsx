caa8588bab551db90fe54062d29e86bc9cedc6e5
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-06 05:17:46 +0500

    Update index.tsx

--- pages/x/index.tsx
+++ pages/x/index.tsx
@@ -1,3 +1,5 @@
+/* Copyright 2020 Genemator Sakhib. All rights reserved. MPL-2.0 license. */
+
 import React, { useMemo } from "react";
 import Link from "next/link";
 import Header from "../../components/Header";
