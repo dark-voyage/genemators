898d31aeb45538d313029195100101024c37e638
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-06 05:18:59 +0500

    Update database.test.ts

--- util/database.test.ts
+++ util/database.test.ts
@@ -1,3 +1,5 @@
+/* Copyright 2020 Genemator Sakhib. All rights reserved. MPL-2.0 license. */
+
 import db from "../database.json";
 import { GithubDatabaseEntry } from "./registries/github";
 import { NPMDatabaseEntry } from "./registries/npm";
