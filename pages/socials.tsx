8947a957142618ef6be3e495fb163716e73e47ca
Geno Ferollo <genemators@gmail.com>
2020-10-29 03:28:16 +0500

    Update socials.tsx

--- pages/socials.tsx
+++ pages/socials.tsx
@@ -29,6 +29,7 @@ const Socials = () => {
             </div>
           </div>
           <div className="mt-6 grid gap-16 lg:grid-cols-5  lg:row-gap-12 items-center text-center justify-center">
+            <div className="items-center justify-center">
               <a href="https://facebook.com/sakhib.orzklv" key="facebook">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -50,6 +51,8 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
               <a href="https://instagram.com/genemator" key="instagram">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -77,6 +80,8 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
               <a href="https://twitter.com/genemator" key="twitter">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -98,6 +103,8 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
               <a href="https://twitch.com/genemators" key="twitch">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -141,6 +148,8 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
               <a href="https://vk.com/genemator" key="vkontakte">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -156,6 +165,8 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
               <a href="https://t.me/genemator" key="telegram">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -171,6 +182,8 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
               <a href="https://github.com/genemators" key="github">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -186,6 +199,8 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
               <a href="https://steamcommunity.com/id/genemator" key="steam">
                 <svg
                   className="h-36 w-36 animated-social"
@@ -201,6 +216,41 @@ const Socials = () => {
                   />
                 </svg>
               </a>
+            </div>
+            <div className="items-center justify-center">
+              <a href="https://discord.gg/mZHkCY4" key="discord">
+                <svg
+                  className="h-36 w-36 animated-social"
+                  fill="none"
+                  viewBox="0 0 50 50"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1"
+                    d="M19.952,5.672c-1.904-1.531-4.916-1.79-5.044-1.801c-0.201-0.017-0.392,0.097-0.474,0.281 c-0.006,0.012-0.072,0.163-0.145,0.398c1.259,0.212,2.806,0.64,4.206,1.509c0.224,0.139,0.293,0.434,0.154,0.659 c-0.09,0.146-0.247,0.226-0.407,0.226c-0.086,0-0.173-0.023-0.252-0.072C15.584,5.38,12.578,5.305,12,5.305S8.415,5.38,6.011,6.872 c-0.225,0.14-0.519,0.07-0.659-0.154c-0.14-0.225-0.07-0.519,0.154-0.659c1.4-0.868,2.946-1.297,4.206-1.509 c-0.074-0.236-0.14-0.386-0.145-0.398C9.484,3.968,9.294,3.852,9.092,3.872c-0.127,0.01-3.139,0.269-5.069,1.822 C3.015,6.625,1,12.073,1,16.783c0,0.083,0.022,0.165,0.063,0.237c1.391,2.443,5.185,3.083,6.05,3.111c0.005,0,0.01,0,0.015,0 c0.153,0,0.297-0.073,0.387-0.197l0.875-1.202c-2.359-0.61-3.564-1.645-3.634-1.706c-0.198-0.175-0.217-0.477-0.042-0.675 c0.175-0.198,0.476-0.217,0.674-0.043c0.029,0.026,2.248,1.909,6.612,1.909c4.372,0,6.591-1.891,6.613-1.91 c0.198-0.172,0.5-0.154,0.674,0.045c0.174,0.198,0.155,0.499-0.042,0.673c-0.07,0.062-1.275,1.096-3.634,1.706l0.875,1.202 c0.09,0.124,0.234,0.197,0.387,0.197c0.005,0,0.01,0,0.015,0c0.865-0.027,4.659-0.667,6.05-3.111 C22.978,16.947,23,16.866,23,16.783C23,12.073,20.985,6.625,19.952,5.672z M8.891,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913s1.674,0.857,1.674,1.913S9.816,14.87,8.891,14.87z M15.109,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913c0.924,0,1.674,0.857,1.674,1.913S16.033,14.87,15.109,14.87z"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="items-center justify-center">
+              <a href="https://www.youtube.com/channel/UCuF2yVTeVW6vcwXobvXXvZg" key="youtube">
+                <svg
+                  className="h-36 w-36 animated-social"
+                  fill="none"
+                  viewBox="0 0 50 50"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1"
+                    d="M24.402344 9C17.800781 9 11.601563 9.5 8.300781 10.199219C6.101563 10.699219 4.199219 12.199219 3.800781 14.5C3.402344 16.898438 3 20.5 3 25C3 29.5 3.398438 33 3.898438 35.5C4.300781 37.699219 6.199219 39.300781 8.398438 39.800781C11.902344 40.5 17.898438 41 24.5 41C31.101563 41 37.097656 40.5 40.597656 39.800781C42.800781 39.300781 44.699219 37.800781 45.097656 35.5C45.5 33 46 29.402344 46.097656 24.902344C46.097656 20.402344 45.597656 16.800781 45.097656 14.300781C44.699219 12.101563 42.800781 10.5 40.597656 10C37.097656 9.5 31 9 24.402344 9 Z M 24.402344 11C31.601563 11 37.398438 11.597656 40.199219 12.097656C41.699219 12.5 42.898438 13.5 43.097656 14.800781C43.699219 18 44.097656 21.402344 44.097656 24.902344C44 29.199219 43.5 32.699219 43.097656 35.199219C42.800781 37.097656 40.800781 37.699219 40.199219 37.902344C36.597656 38.601563 30.597656 39.097656 24.597656 39.097656C18.597656 39.097656 12.5 38.699219 9 37.902344C7.5 37.5 6.300781 36.5 6.101563 35.199219C5.300781 32.398438 5 28.699219 5 25C5 20.398438 5.402344 17 5.800781 14.902344C6.101563 13 8.199219 12.398438 8.699219 12.199219C12 11.5 18.101563 11 24.402344 11 Z M 19 17L19 33L33 25 Z M 21 20.402344L29 25L21 29.597656Z"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
