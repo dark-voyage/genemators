ecaeb01a1d939aec134fed78f02c27b828ed8f2e
Geno Ferollo <genemators@gmail.com>
2020-10-29 02:50:53 +0500

    circuit background

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             stroke="#fff"
             strokeLinecap="round"
           >
-            <circle cx="80" cy="80" r="50" fill="#fff"/>
+            <circle cx="80" cy="80" r="50" fill="#fff" />
             <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
               <path
                 d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z"
