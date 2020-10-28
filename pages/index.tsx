adcd15c938e334cb043653928a3a1e2eccdbe98a
Geno Ferollo <genemators@gmail.com>
2020-10-29 02:46:41 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
             <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
               <path
                 d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z"
-                fill="#FFF"
+                fill="#808080"
               >
                 <animateTransform
                   attributeName="transform"
