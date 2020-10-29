aee188e129263ab2d49f08c37d9650358c392e9e
Geno Ferollo <genemators@gmail.com>
2020-10-29 21:51:06 +0500

    Update index.tsx

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -1,7 +1,15 @@
 // Fake users data
 import { NextApiRequest, NextApiResponse } from "next";
 
-const message = [{ status: "ok" }];
+const message = [
+    {
+      status: "ok",
+      links: {
+        home: "/api",
+        posts: "/posts"
+      }
+    }
+  ];
 
 export default function apiHandler(req: NextApiRequest, res: NextApiResponse) {
   // Get data from your database
