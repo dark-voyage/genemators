56ce7e3885017e297a862089ed2f97a5d5d053bd
Geno Ferollo <genemators@gmail.com>
2020-10-29 22:25:18 +0500

    Create [post].tsx

---
+++ pages/api/posts/[post].tsx
@@ -0,0 +1,22 @@
+/* Copyright 2020 Genemator Sakhib. All rights reserved. MPL-2.0 license. */
+
+import React from "react";
+
+import { NextApiRequest, NextApiResponse, } from "next";
+import { promises as fs } from "fs";
+import { join } from "path";
+
+const postsViewHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const dir = await fs.readdir("./public/posts");
+  const postIds = dir.filter((name) => name.endsWith(".json"));
+  const paths = postIds.map((id) => ({
+    params: { post: id.replace(/\.json$/, "") },
+  }));
+
+  res.status(200).json(paths);
+};
+
+export default postsViewHandler;
