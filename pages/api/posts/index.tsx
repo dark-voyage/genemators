8364b43e3c08c4a0d71a55ea54e744fa50f692fc
Geno Ferollo <genemators@gmail.com>
2020-10-31 02:27:07 +0500

    Update index.tsx

--- pages/api/posts/index.tsx
+++ pages/api/posts/index.tsx
@@ -1,7 +1,19 @@
-// Fake users data
-const users = [{ id: 1 }, { id: 2 }, { id: 3 }]
+import { NextApiRequest, NextApiResponse } from "next";
+import { promises } from "fs";
+import { join } from "path";
 
-export default function handler(req, res) {
-  // Get data from your database
-  res.status(200).json(users)
+const postsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const dir = await promises.readdir("./public/posts");
+  const postIds = dir.filter((name) => name.endsWith(".json"));
+  const posts = await Promise.all(
+    postIds.map(async (name) => {
+      const file = await promises.readFile(join("./public/posts", name), {
+        encoding: "utf8",
+      });
+      return { ...JSON.parse(file), id: name.replace(/\.json$/, "") };
+    })
+  );
+  res.status(200).json({ props: posts });
 }
+
+export default postsHandler;
