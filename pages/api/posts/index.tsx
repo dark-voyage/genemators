d68097f5f1b9fbdaba3e5a0328a4132833fcd5c9
Geno Ferollo <genemators@gmail.com>
2020-10-31 02:29:20 +0500

    Update index.tsx

--- pages/api/posts/index.tsx
+++ pages/api/posts/index.tsx
@@ -1,19 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { promises } from "fs";
-import { join } from "path";
+import exp from "constants";
 
-const postsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const dir = await promises.readdir("./public/posts");
-  const postIds = dir.filter((name) => name.endsWith(".json"));
-  const posts = await Promise.all(
-    postIds.map(async (name) => {
-      const file = await promises.readFile(join("./public/posts", name), {
-        encoding: "utf8",
-      });
-      return { ...JSON.parse(file), id: name.replace(/\.json$/, "") };
-    })
-  );
-  res.status(200).json({ props: posts });
+const users = [{ id: 1 }, { id: 2 }, { id: 3 }];
+
+export default function postsHandler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json(users);
 }
-
-export default postsHandler;
