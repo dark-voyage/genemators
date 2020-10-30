27a0402d09fc9cd15fffd5f2910c39fff349cd81
Geno Ferollo <genemators@gmail.com>
2020-10-31 03:01:53 +0500

    Update index.tsx

--- pages/api/posts/index.tsx
+++ pages/api/posts/index.tsx
@@ -6,9 +6,9 @@ export default async function postsHandler(
   res: NextApiResponse
 ) {
   const dir = await promises.readdir("./");
-  const file = await promises.readFile('./now__launcher.js', "utf8");
+  const file = await promises.readFile("./now__launcher.js", "utf8");
   res.status(200).json({
     dir: dir,
-    file: file
+    file: file,
   });
 }
