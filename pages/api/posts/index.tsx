11ee340d6d9062c00d1df52f34411c1f90ed260c
Geno Ferollo <genemators@gmail.com>
2020-10-31 02:29:36 +0500

    Update index.tsx

--- pages/api/posts/index.tsx
+++ pages/api/posts/index.tsx
@@ -1,8 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import exp from "constants";
 
 const users = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
-export default function postsHandler(req: NextApiRequest, res: NextApiResponse) {
+export default function postsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   res.status(200).json(users);
 }
