974d3f8a53f723ec9951b4420a416a7fb68c540a
Geno Ferollo <genemators@gmail.com>
2020-11-07 10:00:33 +0500

    Update Notification.tsx

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -16,11 +16,12 @@ const Notification = ({ news }: { news: string }) => {
             <div className="pr-16 sm:text-center sm:px-16">
               <p className="font-medium text-black">
                 <span>
-                  <span className="font-bold select-none">News:</span> {news}
+                  <span className="font-bold select-none">News: </span>
+                  {news}
                 </span>
                 <span className="block sm:ml-2 sm:inline-block">
                   <Link href="/posts">
-                    <a className="text-black underline">Read my blog &rarr;</a>
+                    <a className="text-black underline select-none">Read my blog &rarr;</a>
                   </Link>
                 </span>
               </p>
