d72996fe5fbfe7e1a8dab5a5a0304eceefd744c2
Geno Ferollo <genemators@gmail.com>
2020-12-03 04:19:46 +0500

    Update bot.ts

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -21,81 +21,90 @@ export default async function telegram(
      * Commands
      */
     bot.start(async (ctx: TelegrafContext) => {
-      await ctx.replyWithHTML(`<b>Welcome to Genemator's Assistant!</b>` +
-        `\n` +
-        `\n` +
-        `This bot helps you to manage with information about Genemator.` +
-        `\n` +
-        `With the help of this bot you can do:` +
-        `\n` +
-        `\n` +
-        `<code>* Check current status of the Genemator</code>` +
-        `\n` +
-        `<code>* Check statuses of github repositories</code>` +
-        `\n` +
-        `\n` +
-        `<i>In order to see full detailed usage information of the bot, press the button below.</i>`, {
-        parse_mode: "HTML",
-        reply_markup: Markup.inlineKeyboard([
-          [Markup.callbackButton("Show detailed information", "help")],
-        ]),
-      });
+      await ctx.replyWithHTML(
+        `<b>Welcome to Genemator's Assistant!</b>` +
+          `\n` +
+          `\n` +
+          `This bot helps you to manage with information about Genemator.` +
+          `\n` +
+          `With the help of this bot you can do:` +
+          `\n` +
+          `\n` +
+          `<code>* Check current status of the Genemator</code>` +
+          `\n` +
+          `<code>* Check statuses of github repositories</code>` +
+          `\n` +
+          `\n` +
+          `<i>In order to see full detailed usage information of the bot, press the button below.</i>`,
+        {
+          parse_mode: "HTML",
+          reply_markup: Markup.inlineKeyboard([
+            [Markup.callbackButton("Show detailed information", "help")],
+          ]),
+        }
+      );
     });
     bot.help(async (ctx: TelegrafContext) => {
-      await ctx.replyWithHTML(`<b>List of available commands:</b>` +
-        `\n` +
-        `\n` +
-        `/help - <code>show this helper message</code>` +
-        `\n` +
-        `\n` +
-        `<i>In order to use our inline mode, switch to inline mode ` +
-        `by typing: @genemabot and then start typing something there.</i>`, {
-        parse_mode: "HTML",
-        reply_markup: Markup.inlineKeyboard([
-          [Markup.switchToCurrentChatButton("Search Projects", "")],
-        ]),
-      });
+      await ctx.replyWithHTML(
+        `<b>List of available commands:</b>` +
+          `\n` +
+          `\n` +
+          `/help - <code>show this helper message</code>` +
+          `\n` +
+          `\n` +
+          `<i>In order to use our inline mode, switch to inline mode ` +
+          `by typing: @genemabot and then start typing something there.</i>`,
+        {
+          parse_mode: "HTML",
+          reply_markup: Markup.inlineKeyboard([
+            [Markup.switchToCurrentChatButton("Search Projects", "")],
+          ]),
+        }
+      );
     });
     bot.command("about", async (ctx: TelegrafContext) => {
-      await ctx.replyWithHTML(`<b>Senior Developer from Uzbekistan's Top 2 ¯\\_(ツ)_/¯</b>` +
-        `\n` +
-        `\n` +
-        `Hello dear visitor! I’m Sokhibjon Orzikulov, who is famous under the nickname Genemator. I’m glad to see you visit my website. For the beginning, let me introduce myself. I’m a Senior Full-Stack developer with more than 7 years of experience. Although I professionally code using 7 different programming languages as JavaScript, TypeScript, Java, Kotlin, C, C++, Assembler, Python2, Python3. I mainly use C++ for its total control and wide range of functions. That said, my knowledge is not limited to coding. I am also a specialist in creating 3D driven video and photo. Besides, I prefer Cinema 4D as 3D engine and utilize Adobe Products for editing purposes. I can also create different songs and music for my projects by using special applications on a professional level. For the time being, I am a second-year student at Westminster International University in Tashkent. My nationality is Uzbek and I’m only 19 years old 😎` +
-        `\n` +
-        `<b>DIRECTIONS THAT I COVER</b>` +
-        `\n` +
-        `\n` +
-        `<code>* Front End</code>` +
-        `\n` +
-        `<i>With the help of trendy JavaScript frameworks as React, Vue or maybe Next.JS I can build speedy and beautifully designed web apps with out-of-box functionalities.</i>` +
-        `\n` +
-        `<code>* Back End</code>` +
-        `\n` +
-        `<i>I am able to build up my own server from zero to hero with various programming languages and frameworks. For example, Express.js on Node.js, Django or Flask on Python, also Oak on Deno.land</i>` +
-        `\n` +
-        `<code>* Database</code>` +
-        `\n` +
-        `<i>Thanks for database programming knowledge, I have possibilities to create server with fully integrated datasets. As an example, postgresql, mysql, vanilla sql and so on.</i>` +
-        `\n` +
-        `<code>* Scripting</code>` +
-        `\n` +
-        `<i>With my knowledge on C, C++ or Python programming languages, I can build ready setup & build scripts, startups or unit tests for project.</i>` +
-        `\n` +
-        `\n` +
-        `<i>In order to see full detailed usage information of the bot, press the button below.</i>`, {
-        parse_mode: "HTML"
-      });
+      await ctx.replyWithHTML(
+        `<b>Senior Developer from Uzbekistan's Top 2 ¯\\_(ツ)_/¯</b>` +
+          `\n` +
+          `\n` +
+          `Hello dear visitor! I’m Sokhibjon Orzikulov, who is famous under the nickname Genemator. I’m glad to see you visit my website. For the beginning, let me introduce myself. I’m a Senior Full-Stack developer with more than 7 years of experience. Although I professionally code using 7 different programming languages as JavaScript, TypeScript, Java, Kotlin, C, C++, Assembler, Python2, Python3. I mainly use C++ for its total control and wide range of functions. That said, my knowledge is not limited to coding. I am also a specialist in creating 3D driven video and photo. Besides, I prefer Cinema 4D as 3D engine and utilize Adobe Products for editing purposes. I can also create different songs and music for my projects by using special applications on a professional level. For the time being, I am a second-year student at Westminster International University in Tashkent. My nationality is Uzbek and I’m only 19 years old 😎` +
+          `\n` +
+          `<b>DIRECTIONS THAT I COVER</b>` +
+          `\n` +
+          `\n` +
+          `<code>* Front End</code>` +
+          `\n` +
+          `<i>With the help of trendy JavaScript frameworks as React, Vue or maybe Next.JS I can build speedy and beautifully designed web apps with out-of-box functionalities.</i>` +
+          `\n` +
+          `<code>* Back End</code>` +
+          `\n` +
+          `<i>I am able to build up my own server from zero to hero with various programming languages and frameworks. For example, Express.js on Node.js, Django or Flask on Python, also Oak on Deno.land</i>` +
+          `\n` +
+          `<code>* Database</code>` +
+          `\n` +
+          `<i>Thanks for database programming knowledge, I have possibilities to create server with fully integrated datasets. As an example, postgresql, mysql, vanilla sql and so on.</i>` +
+          `\n` +
+          `<code>* Scripting</code>` +
+          `\n` +
+          `<i>With my knowledge on C, C++ or Python programming languages, I can build ready setup & build scripts, startups or unit tests for project.</i>` +
+          `\n` +
+          `\n` +
+          `<i>In order to see full detailed usage information of the bot, press the button below.</i>`,
+        {
+          parse_mode: "HTML",
+        }
+      );
     });
     bot.action("help", async (ctx: TelegrafContext) => {
       await ctx.editMessageText(
         `<b>List of available commands:</b>` +
-        `\n` +
-        `\n` +
-        `/help - <code>show this helper message</code>` +
-        `\n` +
-        `\n` +
-        `<i>In order to use our inline mode, switch to inline mode ` +
-        `by typing: @genemabot and then start typing something there.</i>`,
+          `\n` +
+          `\n` +
+          `/help - <code>show this helper message</code>` +
+          `\n` +
+          `\n` +
+          `<i>In order to use our inline mode, switch to inline mode ` +
+          `by typing: @genemabot and then start typing something there.</i>`,
         {
           parse_mode: "HTML",
           reply_markup: Markup.inlineKeyboard([
@@ -115,7 +124,11 @@ export default async function telegram(
         indexation = 1,
         // @ts-ignore
         repos = Object.values(
-          await fetch("https://api.github.com/users/genemators/repos").then(r => {return r.json()})
+          await fetch("https://api.github.com/users/genemators/repos").then(
+            (r) => {
+              return r.json();
+            }
+          )
         ).map(function (obj) {
           // @ts-ignore
           return obj["name"];
@@ -129,7 +142,11 @@ export default async function telegram(
         return obj.string;
       });
       for (let key of found) {
-        let data = await fetch(`https://api.github.com/repos/genemators/${key}`).then(r => {return r.json()});
+        let data = await fetch(
+          `https://api.github.com/repos/genemators/${key}`
+        ).then((r) => {
+          return r.json();
+        });
         results.push({
           type: "article",
           id: indexation,
@@ -149,7 +166,8 @@ export default async function telegram(
             { columns: 2 }
           ),
           input_message_content: {
-            message_text: `<b><a href="${data["html_url"]}">〰 GitHub Project Review 〰</a></b>` +
+            message_text:
+              `<b><a href="${data["html_url"]}">〰 GitHub Project Review 〰</a></b>` +
               `\n` +
               `\n` +
               `<b>Description:</b> ${data["description"]}` +
