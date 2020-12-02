e6eb345833ea418bd3714eccedb340d783bb4c7e
Geno Ferollo <genemators@gmail.com>
2020-12-02 14:15:42 +0500

    Update bot.ts

--- bot/core/bot.ts
+++ bot/core/bot.ts
@@ -8,5 +8,5 @@ export const initialization = async () => {
   bot.start(async (ctx: TelegrafContext) => await start(ctx));
   bot.help(async (ctx: TelegrafContext) => await help(ctx));
   bot.action("help", async (ctx: TelegrafContext) => await helpAction(ctx));
-  bot.on("inline_query", async (ctx: TelegrafContext) => await inline(ctx));
+  // bot.on("inline_query", async (ctx: TelegrafContext) => await inline(ctx));
 };
