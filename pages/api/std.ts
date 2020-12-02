6dbb4e9a83273dd218115f1f38af54d71117ff5c
Geno Ferollo <genemators@gmail.com>
2020-12-02 14:04:15 +0500

    Update std.ts

--- pages/api/std.ts
+++ pages/api/std.ts
@@ -1,7 +1,15 @@
+import {name, description, version, bugs} from "../../package.json"
 import { NextApiResponse, NextApiRequest } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ url: process.env.VERCEL_URL }));
+  res.end(JSON.stringify(
+    {
+      name: name,
+      about: description,
+      version: version,
+      bugs: bugs
+    })
+  );
 }
