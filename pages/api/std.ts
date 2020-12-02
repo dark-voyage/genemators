9d9d18732fd21eb59b2c4f826edbbb469292801d
Geno Ferollo <genemators@gmail.com>
2020-12-02 14:04:24 +0500

    Update std.ts

--- pages/api/std.ts
+++ pages/api/std.ts
@@ -1,15 +1,15 @@
-import {name, description, version, bugs} from "../../package.json"
+import { name, description, version, bugs } from "../../package.json";
 import { NextApiResponse, NextApiRequest } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(
-    {
+  res.end(
+    JSON.stringify({
       name: name,
       about: description,
       version: version,
-      bugs: bugs
+      bugs: bugs,
     })
   );
 }
