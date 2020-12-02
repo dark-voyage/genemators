ffde944c1717cd0fda5d387fbdb7b23f7d79ce0e
Geno Ferollo <genemators@gmail.com>
2020-12-02 15:42:50 +0500

    Update request.ts

--- functions/lib/request.ts
+++ functions/lib/request.ts
@@ -1,4 +1,4 @@
-export default async function (link: RequestInfo): Promise<JSON> {
+export default async function (link: RequestInfo): Promise<any> {
   return await fetch(link).then((res: Response) => {
     return res.json();
   });
