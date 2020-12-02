346460ffc6a0d922f1113bc8fb28d197488072c8
Geno Ferollo <genemators@gmail.com>
2020-12-02 14:02:05 +0500

    commands added again

--- bot/core/bot.ts
+++ bot/core/bot.ts
@@ -6,9 +6,7 @@ export const bot = new Telegraf<TelegrafContext>(<string>process.env.BOT_TOKEN);
 
 export const initialization = async () => {
   bot.start(async (ctx: TelegrafContext) => await start(ctx));
-  bot.on("text", ctx => ctx.reply("Halo"))
-  // bot
-  //   .help(async (ctx: TelegrafContext) => await help(ctx))
-  //   .action("help", async (ctx: TelegrafContext) => await helpAction(ctx));
-  // bot.on("inline_query", async (ctx: TelegrafContext) => await inline(ctx));
+  bot.help(async (ctx: TelegrafContext) => await help(ctx));
+  bot.action("help", async (ctx: TelegrafContext) => await helpAction(ctx));
+  bot.on("inline_query", async (ctx: TelegrafContext) => await inline(ctx));
 };
