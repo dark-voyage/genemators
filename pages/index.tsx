137ea081eba0fdcf6986478860d58069910b2d57
Geno Ferollo <genemators@gmail.com>
2020-12-11 10:03:39 +0500

    read and formation

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,36 +24,38 @@ export default function Home() {
       <Notification news={news} />
       <div className="h-auto">
         <div className="flex justify-center items-center">
-          <svg
-            className="text-white xl:w-3/4 lg:w-3/4 sm:w-3/4 w-3/4"
-            version="1.1"
-            width="450"
-            height="450"
-            viewBox="0 0 160 160"
-            fill="none"
-          >
-            <circle cx="80" cy="80" r="50" fill="#fff" />
-            <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
+          <a href="https://i.am.genemator.me">
+            <svg
+              className="text-white xl:w-3/4 lg:w-3/4 sm:w-3/4 w-3/4"
+              version="1.1"
+              width="450"
+              height="450"
+              viewBox="0 0 160 160"
+              fill="none"
+            >
+              <circle cx="80" cy="80" r="50" fill="#fff" />
+              <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
+                <path
+                  d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z"
+                  fill="#808080"
+                >
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    from="360 0 0"
+                    to="0 0 0"
+                    dur="1s"
+                    repeatCount="indefinite"
+                  />
+                </path>
+              </g>
               <path
-                d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z"
-                fill="#808080"
-              >
-                <animateTransform
-                  attributeName="transform"
-                  type="rotate"
-                  from="360 0 0"
-                  to="0 0 0"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </path>
-            </g>
-            <path
-              d="M 50,0 A 50,50 0 0,0 -50,0Z"
-              fill="#fff"
-              transform="matrix(0.866, -0.5, 0.5, 0.866, 80, 80)"
-            />
-          </svg>
+                d="M 50,0 A 50,50 0 0,0 -50,0Z"
+                fill="#fff"
+                transform="matrix(0.866, -0.5, 0.5, 0.866, 80, 80)"
+              />
+            </svg>
+          </a>
         </div>
         <div className="flex mb-4 justify-center">
           <div className="genemator-title px-3 text-white text-center xl:text-6xl lg:text-5xl all:text-5xl text-5xl border-4 rounded-lg">
