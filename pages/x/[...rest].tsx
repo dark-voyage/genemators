3f4ffbea8d6af975f7bfb5cd8fe0ff7c325d8108
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-02-09 01:09:29 +0500

    cleaning up project

--- pages/x/[...rest].tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Registry from "../../components/Registry";
-
-function RegistryPage(): React.ReactElement {
-  return <Registry />;
-}
-
-export default RegistryPage;
